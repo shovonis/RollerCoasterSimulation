637de9dce2a2e930037c740c3af8cf8e18698118
Samuel Ang <samuel.ang.prog@gmail.cmo>
2020-06-12 14:58:13 -0500

    Added Blurring Technique
    Added dynamic blurrign technique which communicates with. Make sure to uncomment the start of the coroutine in the communication script to run properly.

---
+++ Assets/BlurPredictionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetworkAPI;
+public class BlurPredictionHandler : MonoBehaviour
+{
+    [Header("Values")]
+    [Range(0f,10f)]
+
+    public float prediction = 0f;
+
+    public float minSigma = .3f;
+    public float maxSigma = 4f;
+    public DynamicGaussianBlur dynamicGaussianBlur;
+    public NetWorkManager networkManager;
+    void Start(){
+        //StartCoroutine(PredictionUpdate());
+    }
+    void Update(){
+        dynamicGaussianBlur.sigma = Mathf.Lerp(minSigma,maxSigma,prediction/10f);
+    }
+
+    float changeRate = .15f;
+    IEnumerator PredictionUpdate(){
+        while(true){
+            prediction = Mathf.Lerp(prediction,networkManager.PredictedCyberSickness,changeRate);
+            yield return null;
+        }
+    }
+}
